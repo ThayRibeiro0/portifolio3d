22779a2fbf94df259c38a9c1da3f1ff6bce63ef2
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2023-04-30 12:46:25 -0700

    9

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+// import Tilt from "react-tilt";
+import { motion } from "framer-motion";
 
-const Works = () => {
-  return (
-    <div>Works</div>
+import { styles } from "../styles";
+// import { github } from '../assets';
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from '../utils/motion';
+
+const ProjectCard = ({ 
+  index, 
+  name, 
+  description, 
+  tags, 
+  image, 
+  source_code_link 
+}) => {
+  return(
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      {/* <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+      }}
+        className='bg-tertiary p-5 rounded-2x1 sm:w-[360px] w-full'
+      >
+        <div className='relative w-full h-[230px]'>
+          <img 
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2x1"
+          />
+        </div>
+      </Tilt> */}
+    </motion.div>
   )
 }
 
-export default Works
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          My works
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Projects 📝
+        </h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3x1 leanding-[30px]"
+        >
+        This section have I bring the projects showcase my skills and experience on this journey in the world of technology. To better understanding I put description, link with the code repository and live demos in it. All this tasks reflects all my abilities in solve problems, in different technologies and manage projects with effectivity.  
+        </motion.p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project}/>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "");
--- src/constants/index.js
+++ src/constants/index.js
@@ -45,10 +45,6 @@ import {
       title: "System Developer",
       icon: web, mobile,
     },
-    {
-      title: "Junior Developer",
-      icon: web, mobile,
-    },
   ];
   
   const technologies = [
@@ -65,16 +61,8 @@ import {
       icon: javascript,
     },
     {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
       name: "React JS",
       icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
     },
     {
       name: "Tailwind CSS",
@@ -108,57 +96,57 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
-      iconBg: "#383E56",
-      date: "March 2020 - April 2021",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
+      title: "Vacancy open for a special company",
+      // company_name: "Starbucks",
+      // icon: starbucks,
+      // iconBg: "#383E56",
+      // date: "March 2020 - April 2021",
+      // points: [
+      //   "Developing and maintaining web applications using React.js and other related technologies.",
+      //   "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      //   "Implementing responsive design and ensuring cross-browser compatibility.",
+      //   "Participating in code reviews and providing constructive feedback to other developers.",
+      // ],
     },
-    {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
-      iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
+    // {
+    //   title: "React Native Developer",
+    //   company_name: "Tesla",
+    //   icon: tesla,
+    //   iconBg: "#E6DEDD",
+    //   date: "Jan 2021 - Feb 2022",
+    //   points: [
+    //     "Developing and maintaining web applications using React.js and other related technologies.",
+    //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //     "Implementing responsive design and ensuring cross-browser compatibility.",
+    //     "Participating in code reviews and providing constructive feedback to other developers.",
+    //   ],
+    // },
+    // {
+    //   title: "Web Developer",
+    //   company_name: "Shopify",
+    //   icon: shopify,
+    //   iconBg: "#383E56",
+    //   date: "Jan 2022 - Jan 2023",
+    //   points: [
+    //     "Developing and maintaining web applications using React.js and other related technologies.",
+    //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //     "Implementing responsive design and ensuring cross-browser compatibility.",
+    //     "Participating in code reviews and providing constructive feedback to other developers.",
+    //   ],
+    // },
+    // {
+    //   title: "Full stack Developer",
+    //   company_name: "Meta",
+    //   icon: meta,
+    //   iconBg: "#E6DEDD",
+    //   date: "Jan 2023 - Present",
+    //   points: [
+    //     "Developing and maintaining web applications using React.js and other related technologies.",
+    //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //     "Implementing responsive design and ensuring cross-browser compatibility.",
+    //     "Participating in code reviews and providing constructive feedback to other developers.",
+    //   ],
+    // },
   ];
   
   const testimonials = [
