4eb0842b8db7579484be39a7a4ff771951f7ce68
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2023-05-27 14:03:11 -0700

    kefhie7

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,10 +15,10 @@ const Hero = () => {
         </div>
         <div className="">
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="font-bold text-[#915eff]">Thays</span>
+            Hi, I am <span className="font-bold text-[#915eff]">Thays</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm Lawyer &#9878;, System Developer &#128187;and Administrator &#128202;
+            I have Graduation degree in Law &#9878;, and Association degree in System Developer &#128187;and Administrator &#128202;
           </p>
         </div>
       </div>
